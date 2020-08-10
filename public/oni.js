9a11edcbf8d65139fc8e970f098c559f0daf37b0
portableD <emmanueltemitopedorcas20@gamil.com>
2020-08-10 16:36:23 +0100

    modify

--- public/oni.js
+++ public/oni.js
@@ -29,3 +29,20 @@ navMenu.addEventListener("click",function() {
     navMenu.style.opacity = "0";
     navMenu.style.zIndex = "-2000"
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+	let i;
+	let slides = document.getElementsByClassName("slide-show");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+		slideIndex++;
+	if (slideIndex > slides.length) {
+		slideIndex = 1
+	}  
+	slides[slideIndex-1].style.display = "flex";
+	setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
