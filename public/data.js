b80f0259048715ae2c6986f1ba38f45f394f706e
portableD <emmanueltemitopedorcas20@gamil.com>
2020-08-17 12:57:43 +0100

    new

--- public/data.js
+++ public/data.js
@@ -1,7 +1,7 @@
 let data = [
     {
         name: 'First slippers',
-        category: 'slippers',
+        category: 'slipper',
         price: '2000',
         image: 'home pics/home1.jpg'
     },
@@ -9,68 +9,88 @@ let data = [
         name: 'First shoe',
         category: 'shoe',
         price: '3000',
-        image: 'slippers.jpg'
+        image: 'home pics/home1.jpg'
+
     },
     {
-        name: 'First shoe',
+        name: 'First slippers',
         category: 'slipper',
         price: '3000',
-        image: 'slippers.jpg'
+        image: 'home pics/home1.jpg'
+
     },
     {
-        name: 'First shoe',
-        category: 'slippers',
+        name: 'First slippers',
+        category: 'slipper',
         price: '3000',
-        image: 'slippers.jpg'
-    },
-    {
-        name: 'First shoe',
-        category: 'shoe',
-        price: '3000',
-        image: 'slippers.jpg'
+        image: 'home pics/home1.jpg'
+
     },
     {
         name: 'First shoe',
         category: 'shoe',
         price: '3000',
-        image: 'slippers.jpg'
+        image: 'home pics/home1.jpg'
+
+    },
+    {
+        name: 'First shoe',
+        category: 'shoe',
+        price: '3000',
+        image: 'home pics/home1.jpg'
+
     },
 ]
 
-function getCategory() {
-    let filitered = [];
-    if (window.location.href.includes('shoe')) {
-        filitered = data.filter(item => {
-           return item.category == 'shoe'
-        })
-    }
-    return alert(JSON.stringify(filitered))
-}
-
-getCategory();
-
+// function getCategory() {
+    
+// }
+// document.querySelector('.product-wrapper').innerHTML = getCategory();
 
 function formatItem() {
     let items   = ""
+    if (window.location.href.includes('index')) {
     for ( i = 0; i < data.length; i++) {
-        
+            items += `<div class ="items">
+                            <a href= #>
+                                <div class= "featured-image">
+                                    <img src= "${data[i].image}">
+                                </div>
+                                <div class = "info">
+                                    <span>${data[i].name}</span>
+                                    <span>&#8358 ${data[i].price}</span>
+                                </div>
+                            </a>
+                        </div>`
 
-         items += `<div class ="items">
-                        <a href= #>
-                            <div class= "featured-image">
-                                <img src= "${data[i].image}">
-                            </div>
-                            <div class = "info">
-                                <span>${data[i].name}</span>
-                                <span>&#8358 ${data[i].price}</span>
-                            </div>
-                        </a>
-                    </div>`
-        
+                    
+        } return items;
+    } else if ( window.location.href.includes('shoe')){
+        let filitered = [];
+        filitered = data.filter(item => {
+            return item.category == 'shoe'
+        })
+            alert(JSON.stringify(filitered))  
     
-
+        for ( i = 0; i < filitered.length; i++) {
+            items += `<div class ="items">
+                            <a href= #>
+                                <div class= "featured-image">
+                                    <img src= "${data[i].image}">
+                                </div>
+                                <div class = "info">
+                                    <span>${data[i].name}</span>
+                                    <span>&#8358 ${data[i].price}</span>
+                                </div>
+                            </a>
+                        </div>`
+    
+                    
+        } return items;
     }
-    return items
 }
 
-document.querySelector('#test').innerHTML = formatItem()
+
+document.querySelector('.product-wrapper').innerHTML = formatItem();
+
+
