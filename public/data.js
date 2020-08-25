534d56e6eac5da24850460d0c07608f271bc86a9
portableD <emmanueltemitopedorcas20@gamil.com>
2020-08-25 10:47:40 +0100

    refactored payment slip

--- public/data.js
+++ public/data.js
@@ -333,7 +333,7 @@ let singleItem = data.filter(item => {
                 `
         link.innerHTML = window.location.href.includes('product')?
             `<button type="submit">purchase</button>` :
-             link.style.display = "none"
+             link.style.visibility = "hidden"
 
 
         desc.innerHTML = switchContent(item.desc, link)
@@ -383,6 +383,27 @@ function paymentForm() {
 }
 
 function payWithPaystack() {
-    alert('here')
+    let handler = PaystackPop.setup ({
+        key:"pk_test_82ce23694563611af6015b7bdfc1dd4a1f044acf",
+        email: "emmanueldorcas15@gmail.com",
+        amount: 370000,
+        metadata: {
+            custom_fields:[
+                {
+                    display_name: "Mobile Number",
+                    variable_name: "mobile_number",
+                    value:document.querySelector('.phone').value,
+                }
+            ]
+        },
+        callback: function(response){
+            let message = 'Payment complete! Reference: ' + response.reference;
+            alert(message);
+          },
+        onClose: function () {
+            alert("Transaction cancelled");
+        }
+    });
+       handler.openIframe();
 }
 
